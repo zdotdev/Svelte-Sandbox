b5da913c477e9d1b873fa04d8d6366e82846d571
zdotdev <www.ragaszedrick25@gmail.com>
2024-03-03 22:33:50 +0800

    Added catch all routing and added href function and reactive to reload the window

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <h1 class="text-red-900">Hello MFs</h1>
-<a href="/routingExample" class="underline mx-2 bg-slate-200">routingExample and nestedRouting</a><br>
-<a href="/dynamicRouting/{id}" class="underline mx-2 bg-slate-200">dinamicRouting and nested dynamic routing</a><br>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="m-4">
+    <a href="/routingExample" class="underline m-4 bg-slate-200">routingExample and nestedRouting</a>
+    <a href="/dynamicRouting/{id}" class="underline m-4 bg-slate-200">dinamicRouting and nested dynamic routing</a>
+</div>
---
+++ src/routes/catchAllRouting/[...Example]/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { page } from '$app/stores';
+    import { writable } from 'svelte/store';
+
+    const location = $page.params.Example;
+
+    const locArr = location.split('/');
+    const locArrStore = writable(locArr);
+
+    function handleClick(event) {
+        event.preventDefault();
+
+        locArrStore.set(location.split('/'));
+
+        window.location.href = '/catchAllRouting/feature/content';
+    }
+
+    $: {
+        const locArr = $locArrStore;
+        if (locArr.length === 1) {
+            console.log('The current location is', location);
+        } else if (locArr.length === 2) {
+            console.log('The current location is nested', location);
+        }
+    }
+</script>
+
+<p>Catch all routing will take all the URL parameter after the main (/catchAllRouting) location of the catch all [...catchAll]</p>
+<p>example: /catchAllRouting/start from here, the content will be the same/ and here/ and here/ and so on</p>
+<p class="m-4 bg-orange-200 w-fit">Location: catchAllRouting/{location}</p>
+
+<h1>Here's how to use the catch all routing:</h1>
+
+{#if locArr.length === 1}
+    <p>The current location is {location}</p>
+    <a href="/catchAllRouting/feature/content" on:click={handleClick} class="bg-green-200">Try nested catch all</a>
+{:else if locArr.length === 2}
+    <p>The current location is nested {location}</p>
+{/if}
