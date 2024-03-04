01e0ccc886f28d11bbc27141d4dcd0a5d2b48ce6
zdotdev <www.ragaszedrick25@gmail.com>
2024-03-04 21:16:36 +0800

    Added new goto description

--- src/routes/optionalParameters/[[lang]]/+page.svelte
+++ src/routes/optionalParameters/[[lang]]/+page.svelte
@@ -1,21 +1,27 @@
 <script>
     import { page } from '$app/stores'
-    import { goto } from '$app/navigation' // goto change the content without refreshing the window. href reloads the window when clicked
+    import { goto } from '$app/navigation' // goto change the content of the dom when specific condition/s is or are met.
     const language = {
         en: "Hello",
         es: "Hola",
         fr: "Bonjour"
     }
 
-    const {lang = "en"} = $page.params // To access the url parameter of a dynamic route. Decalring "en" as a default language 
+    let {lang = "en"} = $page.params // To access the url parameter of a dynamic route. Decalring "en" as a default language 
 
-    const greeting = language[lang]
+    let greeting = language[lang]
+
+    $: {
+        const {lang} = $page.params;
+        greeting = language[lang] || greeting;
+    }
+
 </script>
 <p>This is an optinal parameter, you can set default value to show</p>
 
 <p>The default language is English</p>
 
 <p>Example: <span class="bg-red-200">{greeting}</span>!</p>
-<a href="/optionalParameters/es" on:click={(event) => {event.preventDefault(); window.location.reload(); window.location.href = '/optionalParameters/es'}} class="bg-yellow-200 m-4">Esapanish Language</a> // Working
-<button class="bg-yellow-200 m-4" on:click={(event) => { event.preventDefault(); goto('/optionalParameters/fr') }}>French Language</button> // Not working
+<a href="/optionalParameters/es" class="bg-yellow-200 m-4">Esapanish Language</a> <!-- Working -->
+<a href="/optionalParameters/fr" class="bg-yellow-200 m-4">French Language</a> <!--Not working -->
 <button on:click={() => {goto('/')}} class="bg-blue-200">Home</button>
